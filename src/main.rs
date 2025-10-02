75317e37bb7929876e9d5f63e54245a4078daf31
Gabriel de Azevedo <184437223+gabriel-de-azevedo@users.noreply.github.com>
2025-10-02 17:21:07 -0300

    feat: complex data types

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
 fn main() {
-    println!("Hello, world!");
+    let some_text = "some_text";
+    let some_more_text = ", world";
+
+    println!("{some_text}");
+
+    let mut some_text = String::from(some_text);
+    some_text.push_str(some_more_text);
+
+    println!("{some_text}");
 }
