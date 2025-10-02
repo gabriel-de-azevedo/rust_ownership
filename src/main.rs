028ee7bdb62c383a879c2ee2bd9984ff3a709907
Gabriel de Azevedo <184437223+gabriel-de-azevedo@users.noreply.github.com>
2025-10-02 17:24:08 -0300

    docs: add commnet

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ fn main() {
 
     println!("{some_text}");
 
-    let mut some_text = String::from(some_text);
+    let mut some_text = String::from(some_text); // A string literal cannot grow in size, so we need to cast our variable to a String
     some_text.push_str(some_more_text);
 
     println!("{some_text}");
