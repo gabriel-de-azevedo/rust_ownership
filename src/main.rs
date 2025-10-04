3a522e7c1290b9b6897659ed7cbb1e946cdc6375
Gabriel de Azevedo <184437223+gabriel-de-azevedo@users.noreply.github.com>
2025-10-04 07:01:02 -0300

    feat: return an untied value

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,28 @@
-fn main() {
-    let mut s = String::from("hello");
+fn first_word(s: &String) -> usize {
+    let bytes = s.as_bytes();
 
-    let r1 = &s; // no problem
-    let r2 = &s; // no problem
-    println!("{r1} and {r2}");
-    // Variables r1 and r2 will not be used after this point.
+    for (i, &item) in bytes.iter().enumerate() {
+        if item == b' ' {
+            return i;
+        }
+    }
 
-    let r3 = &mut s; // no problem
-    println!("{r3}");
+    s.len()
 }
+
+fn main() {
+    let mut s = String::from("hello world");
+
+    let word = first_word(&s); // word will get the value 5
+
+    s.clear(); // this empties the String, making it equal to ""
+
+    println!("{word}")
+    // word still has the value 5 here, but s no longer has any content that we
+    // could meaningfully use with the value 5, so word is now totally invalid!
+}
+
+// I'm really happy that this is something addressed in the Rust Book
+// It means that me and whoever wrote the book think about code in a similar way
+// This is the first thing I thought of checking
+// It's also very refreshing to work with a language that addresses these things all the way
